bfc46c512bfcb5199d395ed1aec36a5ec70600b3
rafael <rafael.hiss@freieformation.de>
2017-03-14 15:05:34 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-#git pull origin dev_rh
+git pull origin dev_rh
+
 #mkdir -p ../../../../../db_backup
 #drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+#drush sql-drop -y
+#drush sql-cli < 2017-02-01__15-05-29.sql
+
+drush cc all
+
+drush en -y map_view_update
+
+
+
+cd ../../themes/future_history/ && git pull origin master && cd -
