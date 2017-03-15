7e237da1b69f1a45ca413e9cddd38e8efda13288
rafael <rafael.hiss@freieformation.de>
2017-03-15 16:11:19 +0100

    update

--- update.sh
+++ update.sh
@@ -4,12 +4,14 @@ git pull origin dev_rh
 
 #mkdir -p ../../../../../db_backup
 #drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+#drush sql-dump > ../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
 #drush sql-drop -y
 #drush sql-cli < 2017-02-01__15-05-29.sql
 
 drush cc all
 
 drush en -y map_view_update
+drush fr -y map_view_update
 
 
 
