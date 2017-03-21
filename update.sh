4d0207f4bfd87349388833348e6d91f29fc8cc1c
rafael <rafael.hiss@freieformation.de>
2017-03-21 12:56:28 +0100

    WIP

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-git pull origin master
+git pull origin dev
 
 #mkdir -p ../../../../../db_backup
-#drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
 #drush sql-dump > ../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
-#drush sql-drop -y
-#drush sql-cli < 2017-02-01__15-05-29.sql
+drush sql-drop -y
+drush sql-cli < 2017-03-15__15-19-01.sql
 
 drush cc all
 
@@ -15,6 +15,7 @@ drush fr -y map_view_update
 
 
 
-cd ../../themes/future_history/ && git pull origin master && cd -
+cd ../../themes/future_history/ && git pull origin dev && cd -
+cd ../futurehistory/ && git pull origin dev_rh && cd -
 
 drush cc all
